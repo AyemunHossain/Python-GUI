30a32271c6f59933a8b781dc1bf542e3b236235f
Ashik <aharonno2017@gmail.com>
2020-01-31 21:59:03 +0600

    Button added

---
+++ Button.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+def action():
+    print("Action!!!")
+
+
+window = tk.Tk()
+
+window.title('App')
+
+label1 = tk.Label(text="Hello World")
+label1.pack(pady=20)
+
+button1 = tk.Button(text="Click Me",width=10,bg='brown',fg='white',command=action)
+button1.pack()
+
+window.geometry("300x200+10+20")
+window.mainloop()
