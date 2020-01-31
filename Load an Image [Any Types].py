90d73d2d4dad954b03e397d10f4edc97e4521338
Ashik <aharonno2017@gmail.com>
2020-02-01 02:07:28 +0600

    Fixed image issue

--- Load an Image [Any Types].py
+++ Load an Image [Any Types].py
@@ -8,9 +8,9 @@ from PIL import Image, ImageTk
 
 root = tk.Tk()
 root.title("Image")
-root.geometry('600x800')
 
-image1 = ImageTk.PhotoImage(Image.open('picture.jpg'))
+
+image1 = ImageTk.PhotoImage(Image.open('Hacker.jpg'))
 label = tk.Label(root, image=image1).pack()
 
 root.mainloop()
--- Load an Image.py
+++ Load an Image.py
@@ -8,8 +8,22 @@
 from tkinter import *
 root= Tk()
 
-image1 = PhotoImage(file="logo.png")        #By tkinter PhotoImage module you can workwith .png only
+image1 = PhotoImage(file="Hacker.png")        #By tkinter PhotoImage module you can workwith .png only
 
-label = Label(root,image=image1,width=400,height=400,bg="#a7d6dc").pack()
+label = Label(root,image=image1,bg="#a7d6dc").pack()
 
 root.mainloop()
+
+
+#So here's and hack you will find if you run this code with the appropriate image :
+#Hack is : 
+'''
+  If you wanna add any jpg file with tkinter module then all you have to do is 
+  convert your image with appropriate height and width then do it 
+  and sing :
+            I am begining feel like a Hacker good, Hacker good
+            all my code are from front to the back nod, back nod
+            now, who things their lengths are not long enough to Hack-box :) :))
+            They said I Hack like Mr.Robot, so call me elliot.....
+        
+'''
