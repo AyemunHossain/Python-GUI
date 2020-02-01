3638d567c123bd71873e492024c9b23fec046ce9
Ashik <aharonno2017@gmail.com>
2020-02-01 19:40:14 +0600

    Multiple page and Status bar

--- Multiple Page by OOP.py
+++ Multiple Page by OOP.py
@@ -4,44 +4,207 @@
 #;=============================================================
 
 import tkinter as tk
-import time
+
+
+class MainClass(tk.Tk):
+    def __init__(self,*args,**kwargs):
+
+#Initialize the TK
+        tk.Tk.__init__(self,*args,**kwargs)
+
+        container=tk.Frame(self)
+        container.pack(side="top",fill="both",expand=True)
+        container.grid_rowconfigure(0,weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames={}
+
+
+        for F in (PageHome,PageProfile,PageTimeline,PageAbout,PageLike,PageSecurity):
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="news")
+
+        self.show_frame(PageHome)
+
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
+
+class PageHome(tk.Frame):
+    def __init__(self,parent,container):
+
+        tk.Frame.__init__(self,parent)
+        label1 = tk.Label(self,text = "Welcome \n Wish you have FUN! :) ")
+        label1.pack(pady=50,padx=5)
+
+        button1 = tk.Button(self, text="Profile", width=10, command=
+                            lambda:container.show_frame(PageProfile))
+        button1.pack()
+
+        button2 = tk.Button(self, text="Timeline", width=10, command=
+                            lambda:container.show_frame(PageTimeline))
+        button2.pack()
+
+        button3 = tk.Button(self, text="Likes", width=10, command=
+                            lambda:container.show_frame(PageLike))
+        button3.pack()
+
+        button4 = tk.Button(self, text="Security", width=10, command=
+                            lambda:container.show_frame(PageSecurity))
+        button4.pack()
+
+        button5 = tk.Button(self, text="About", width=10, command=
+                            lambda:container.show_frame(PageAbout))
+        button5.pack()
+
+
+class PageProfile(tk.Frame):
+
+    def __init__(self,parent,container):
+
+        tk.Frame.__init__(self,parent)
+        label1 = tk.Label(self, text="Your Profile !!!!")
+        label1.pack(pady=50,padx=5)
+
+        button1 = tk.Button(self, text="Home", width=10, command=
+                            lambda:container.show_frame(PageHome))
+        button1.pack()
+
+        button2 = tk.Button(self, text="About", width=10, command=
+                            lambda:container.show_frame(PageAbout))
+        button2.pack()
+
+        button3 = tk.Button(self, text="Timeline", width=10, command=
+                            lambda:container.show_frame(PageTimeline))
+        button3.pack()
+
+        button4 = tk.Button(self, text="Likes", width=10, command=
+                            lambda:container.show_frame(PageLike))
+        button4.pack()
+
+        button5 = tk.Button(self, text="Security", width=10, command=
+                            lambda:container.show_frame(PageSecurity))
+        button5.pack()
 
 
 
-class FileUpload(tk.Tk):
+class PageAbout(tk.Frame):
 
-    def __init__(self,root,*args,**kwargs):
-        root.geometry('400x400')
+    def __init__(self,parent,container):
 
+        tk.Frame.__init__(self,parent)
+        label1 = tk.Label(self, text="About Yourself :) ")
+        label1.pack(pady=50,padx=5)
 
-        self.label1 = tk.Label(root,text="Upload Your profile picture:").pack(side=tk.TOP)
-        self.button1 = tk.Button(root,text="Upload",width=10,bg="brown",fg="white",command=self.upload_file)
-        self.button1.pack(pady=35)
-        self.label2 = tk.Label(root,text="System Free",relief=tk.SUNKEN,bd=2,anchor=tk.W)
-        self.label2.pack(side=tk.BOTTOM, fill=tk.X,padx=2)
+        button1 = tk.Button(self, text="Profile", width=10, command=
+                            lambda:container.show_frame(PageProfile))
+        button1.pack()
 
+        button2 = tk.Button(self, text="Home", width=10, command=
+                            lambda:container.show_frame(PageHome))
+        button2.pack()
 
+        button3 = tk.Button(self, text="Timeline", width=10, command=
+                            lambda:container.show_frame(PageTimeline))
+        button3.pack()
 
-    def upload_file(self):
-        self.button1.configure(bg="white",state=tk.DISABLED)
-        self.button1.update()
-        self.label2.configure(text="File Uplading ....",fg="green")
-        self.label2.update()
-        time.sleep(1.1)
-        self.label2.configure(text="Wait ....",fg="brown")
-        self.label2.update()
-        time.sleep(1.1)
-        self.status()
+        button4 = tk.Button(self, text="Likes", width=10, command=
+                            lambda:container.show_frame(PageLike))
+        button4.pack()
 
-
-    def status(self):
-        self.label2.configure(text="File Uploaded :)",fg="green")
-        self.label2.update()
-        time.sleep(0.5)
-        self.button1.configure(bg="brown", state=tk.NORMAL)
-        self.button1.update()
+        button5 = tk.Button(self, text="Security", width=10, command=
+                            lambda:container.show_frame(PageSecurity))
+        button5.pack()
 
 
-root = tk.Tk()
-file1 = FileUpload(root)
-root.mainloop()
+class PageTimeline(tk.Frame):
+
+    def __init__(self,parent,container):
+
+        tk.Frame.__init__(self,parent)
+        label1 = tk.Label(self, text="Your recent Posts : ")
+        label1.pack(pady=50,padx=5)
+
+        button1 = tk.Button(self, text="Profile", width=10, command=
+                            lambda:container.show_frame(PageProfile))
+        button1.pack()
+
+        button2 = tk.Button(self, text="About", width=10, command=
+                            lambda:container.show_frame(PageAbout))
+        button2.pack()
+
+        button3 = tk.Button(self, text="Likes", width=10, command=
+                            lambda:container.show_frame(PageLike))
+        button3.pack()
+
+        button4 = tk.Button(self, text="Security", width=10, command=
+                            lambda:container.show_frame(PageSecurity))
+        button4.pack()
+
+        button5 = tk.Button(self, text="Home", width=10, command=
+                            lambda:container.show_frame(PageHome))
+        button5.pack()
+
+
+class PageLike(tk.Frame):
+
+    def __init__(self,parent,container):
+
+        tk.Frame.__init__(self,parent)
+        label1 = tk.Label(self, text="Things you have liked :) ")
+        label1.pack(pady=50,padx=5)
+
+        button1 = tk.Button(self, text="Profile", width=10, command=
+                            lambda:container.show_frame(PageProfile))
+        button1.pack()
+
+        button2 = tk.Button(self, text="Timeline", width=10, command=
+                            lambda:container.show_frame(PageTimeline))
+        button2.pack()
+
+        button3 = tk.Button(self, text="About", width=10, command=
+                            lambda:container.show_frame(PageAbout))
+        button3.pack()
+
+        button4 = tk.Button(self, text="Security", width=10, command=
+                            lambda:container.show_frame(PageSecurity))
+        button4.pack()
+
+        button5 = tk.Button(self, text="Home", width=10, command=
+                            lambda:container.show_frame(PageHome))
+        button5.pack()
+
+
+class PageSecurity(tk.Frame):
+
+    def __init__(self,parent,container):
+
+        tk.Frame.__init__(self,parent)
+        label1 = tk.Label(self, text="Security Issues Here")
+        label1.pack(pady=50,padx=5)
+
+        button1 = tk.Button(self, text="Profile", width=10, command=
+                            lambda:container.show_frame(PageProfile))
+        button1.pack()
+
+        button2 = tk.Button(self, text="Timeline", width=10, command=
+                            lambda:container.show_frame(PageTimeline))
+        button2.pack()
+
+        button3 = tk.Button(self, text="Likes", width=10, command=
+                            lambda:container.show_frame(PageLike))
+        button3.pack()
+
+        button4 = tk.Button(self, text="About", width=10, command=
+                            lambda:container.show_frame(PageAbout))
+        button4.pack()
+
+        button5 = tk.Button(self, text="Home", width=10, command=
+                            lambda: container.show_frame(PageHome))
+        button5.pack()
+
+
+app=MainClass()
+app.mainloop()
