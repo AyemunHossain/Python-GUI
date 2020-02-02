b5536128f8beff62fbec72e547d16703cb367651
Ashik <aharonno2017@gmail.com>
2020-02-02 20:29:21 +0600

    Fixed Spinbox

--- Tkinter Spinbox.py
+++ Tkinter Spinbox.py
@@ -3,16 +3,22 @@
 #; Author: @AyemunHossain
 #;==========================================
 
+import tkinter as tk
+
+root = tk.Tk()
+
+def set_date():
+
+    label= tk.Label(root,text='Select one :').pack()
+    w_val = tk.IntVar()
+    
+    w = tk.Spinbox(root, from_=1, to=30, textvariable=w_val)
+    w.pack()
 
 
-from tkinter import *
+    button2 = tk.Button(root,text="See", width=10,bg="brown",fg='white',
+                        command=lambda : print(w_val.get()))
+    button2.pack()
 
-master = Tk()
-
-w = Spinbox(master, from_=0, to=10)
-w.pack()
-
-button = Button(text="Click me",width=10)
-button.pack()
-
-mainloop()
+set_date()
+root.mainloop()
